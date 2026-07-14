00efa90e2bbfec94b104802c434cdb58a3f8411f
Divyashree Mallarapu <mdivya0212@gmail.com>
2026-07-14 22:15:03 +0530

    Add: three-integer-sum - submission-0

---
+++ Data Structures & Algorithms/three-integer-sum/submission-0.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        nums.sort((a,b)=>a-b)
+        let h = new Set()
+
+        for(let i=0; i<nums.length; i++){
+            let j = i+1
+            let k = nums.length-1
+            while(j<k){
+                if(nums[i]+nums[j]+nums[k]===0){
+                    let temp = []
+                    temp.push(nums[i])
+                    temp.push(nums[j])
+                    temp.push(nums[k])
+                    h.add(temp.join(','))
+                    j++
+                    k--
+                }
+                else if(nums[i]+nums[j]+nums[k]>0){
+                    k--
+                }
+                else{
+                    j++
+                }
+            }
+            
+        }
+        let ans = [...h].map(str=>str.split(',').map(Number))
+        return ans
+    }
+}
